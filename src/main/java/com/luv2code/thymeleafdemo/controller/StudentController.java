fe9406c382eb5c67a162785084326135be53cc2e
Evanilson Pereira <pevanilson99@gmail.com>
2024-04-01 14:26:15 -0300

    Drop down list - PT2

--- src/main/java/com/luv2code/thymeleafdemo/controller/StudentController.java
+++ src/main/java/com/luv2code/thymeleafdemo/controller/StudentController.java
@@ -1,14 +1,21 @@
 package com.luv2code.thymeleafdemo.controller;
 
 import com.luv2code.thymeleafdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+
+    @Value("${countries}")
+    List<String> countries;
+
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel) {
         // create a Student object
@@ -16,6 +23,9 @@ public class StudentController {
 
         // add student object to the model
         theModel.addAttribute("student", theStudent);
+
+        // add the list of countries to the model
+        theModel.addAttribute("countries", countries);
 
         return "student-form";
     }
